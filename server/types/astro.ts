8ed8c2246159a9605877f4aa64dc997df7773789
Nathan St.Amand <nathansaint810@gmail.com>
2024-12-02 21:15:44 -0600

    Add API types

---
+++ server/types/astro.ts
@@ -0,0 +1,10 @@
+export type Astro = {
+  sunrise: string;
+  sunset: string;
+  moonrise: string;
+  moonset: string;
+  moon_phase: string;
+  moon_illumination: number;
+  is_moon_up: 0 | 1;
+  is_sun_up: 0 | 1;
+}
