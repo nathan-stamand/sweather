8a04fdedd4c06a017baa0473907ef4963c7b3a35
Nathan St.Amand <nathansaint810@gmail.com>
2024-12-03 06:49:40 -0600

    Rename file and set up for cors / modularization

---
+++ server/index.ts
@@ -0,0 +1,36 @@
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4';
+import express from 'express';
+import http from 'http';
+import cors from 'cors'
+import { resolvers, typeDefs } from './schema/index.mjs';
+
+const port = 8000;
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  '/graphql',
+  cors<cors.CorsRequest>({ origin: ['http://localhost:3000'] }),
+  express.json(),
+  expressMiddleware(server),
+)
+
+console.log('what da hell')
+
+await new Promise<void>((resolve) => httpServer.listen({
+  port
+}, resolve));
+
+console.log(`Server running at http://localhost:${port}`)
+
+export default {}
