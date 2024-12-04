d0216331cac93cd5fbbbb40342ba1c4446a4e399
Nathan St.Amand <nathansaint810@gmail.com>
2024-12-04 17:49:45 -0600

    Use getWeather method in resolver, remove placeholder

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -1,12 +1,10 @@
 import { location } from "../tests/location.ts"
 
-const weather = {
-  location: location,
-}
-
 export const resolvers = {
   Query: {
-    weather: () => weather,
+    weather: async (_, { search }, { dataSources }) => {
+      return dataSources.weatherAPI.getWeather(search);
+    },
     locations: async (_, { search }, { dataSources }) => {
       return dataSources.weatherAPI.getLocations(search);
     },
