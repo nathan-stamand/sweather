8a50a3368f742b02d0d513c5d162b20222067fcc
Nathan St.Amand <nathansaint810@gmail.com>
2024-12-04 21:30:07 -0600

    Remove types I'm not using

--- client/src/types/current.ts
+++ client/src/types/current.ts
@@ -1,35 +1,37 @@
 import { Condition } from "./condition";
 
-export type Current = {
-  current: {
-    last_updated_epoch: number;
-    last_updated: string;
-    temp_c: number;
-    temp_f: number;
-    is_day: 0 | 1;
-    condition: Condition;
-    wind_mph: number;
-    wind_kph: number;
-    wind_degree: number
-    wind_dir: string;
-    pressure_mb: string;
-    pressure_in: string;
-    precip_mm: number;
-    precip_in: number;
-    humidity: number;
-    cloud: number;
-    feelslike_c: number;
-    feelslike_f: number;
-    windchill_c: number;
-    windchill_f: number;
-    heatindex_c: number;
-    heatindex_f: number;
-    dewpoint_c: number;
-    dewpoint_f: number;
-    vis_km: number;
-    vis_miles: number;
-    uv: number;
-    gust_mph: number;
-    gust_kph: number;
-  }
+type CustomProperties = {
+  temperature: number;
+}
+
+export type Current = Partial<CustomProperties> & {
+  last_updated_epoch: number;
+  last_updated: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: 0 | 1;
+  condition: Condition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number
+  wind_dir: string;
+  pressure_mb: string;
+  pressure_in: string;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  windchill_c: number;
+  windchill_f: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  dewpoint_c: number;
+  dewpoint_f: number;
+  vis_km: number;
+  vis_miles: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
 }
