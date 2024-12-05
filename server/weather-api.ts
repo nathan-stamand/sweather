f669b07bf8330f62ac1d64ba76d9efb302a3e29a
Nathan St.Amand <nathansaint810@gmail.com>
2024-12-04 20:12:35 -0600

    Add custom current implementation to weather api

--- server/weather-api.ts
+++ server/weather-api.ts
@@ -2,8 +2,12 @@ import { RESTDataSource } from '@apollo/datasource-rest';
 import { Location } from './types/location';
 import { Weather } from './types/weather';
 
+interface WeatherAPIStructure {
+  getLocations: (query: string) => Promise<Location[]>;
+  getWeather: (query: string, fahrenheit: boolean, imperial: boolean) => Promise<Weather>;
+}
 
-export class WeatherAPI extends RESTDataSource {
+export class WeatherAPI extends RESTDataSource implements WeatherAPIStructure {
   override baseURL = process.env.WEATHER_BASE_URL;
 
   async getLocations(query: string): Promise<Location[]> {
@@ -13,12 +17,17 @@ export class WeatherAPI extends RESTDataSource {
     return this.get<Location[]>(loc.href);
   }
 
-  async getWeather(query: string): Promise<Weather> {
+  async getWeather(query: string) {
     const loc = new URL(this.baseURL + '/forecast.json');
     loc.searchParams.set('key', process.env.WEATHER_API_KEY);
     loc.searchParams.set('q', encodeURIComponent(query));
     loc.searchParams.set('days', '3');
-    console.log(loc.href)
-    return this.get<Weather>(loc.href);
+    const response = await this.get<Weather>(loc.href);
+    const { current } = response;
+    current.temperature = current.temp_f;
+    console.log(current)
+
+    response.current = current;
+    return response;
   }
 }
